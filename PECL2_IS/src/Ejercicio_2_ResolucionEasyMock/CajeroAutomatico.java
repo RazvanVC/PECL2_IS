864579ba9cee4de2104c6c04d3890d25fa024069
RazvanVC <razvanvc@yahoo.com>
2020-05-01 13:41:32 +0200

    Mod ECupon y EOferta

--- PECL2_IS/src/Ejercicio_2_ResolucionEasyMock/CajeroAutomatico.java
+++ PECL2_IS/src/Ejercicio_2_ResolucionEasyMock/CajeroAutomatico.java
@@ -9,6 +9,40 @@ package Ejercicio_2_ResolucionEasyMock;
  *
  * @author razvanvc
  */
+/**
+ * Simula un cajero automático.
+ */
 public class CajeroAutomatico {
-    
+
+    private String _codigo = null;
+    /**
+     * La cuenta corriente sobre la que se opera en una sesión.
+     */
+    private String _cuenta;
+
+    public CajeroAutomatico(String codigo) {
+        _codigo = codigo;
+    }
+
+    public void iniciarSesion(String ccc) {
+        assert (_cuenta == null);
+        _cuenta = ccc; //cuenta del cliente 
+    }
+
+    public void cerrarSesion() {
+        assert (_cuenta != null);
+        _cuenta = null; //pone a null la cuenta 
+    }
+
+    public boolean realizarRetirada(double cantidad, PasarelaPago p) {
+        assert (_cuenta != null);
+        if (p.estaBloqueada()) { //Si está bloqueada nada 
+            return false;
+        }
+        if (p.tieneSaldo(cantidad)) { //Si está activa y tiene saldo
+            p.retirar(cantidad);
+            return true;
+        }
+        return false;
+    }
 }
