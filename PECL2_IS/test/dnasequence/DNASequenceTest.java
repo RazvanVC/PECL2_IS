387d8a306e83e103af74327d733f0ca943cb2f8c
RazvanVC <razvanvc@yahoo.com>
2020-05-08 12:32:13 +0200

    Bueno

--- PECL2_IS/test/dnasequence/DNASequenceTest.java
+++ PECL2_IS/test/dnasequence/DNASequenceTest.java
@@ -5,7 +5,6 @@
  */
 package dnasequence;
 
-
 import Ejercicio_1_dnasequence.DNASequence;
 import org.junit.After;
 import org.junit.Test;
@@ -17,48 +16,59 @@ import org.junit.Before;
  * @author azaha
  */
 public class DNASequenceTest {
-private static DNASequence seq;
-private static DNASequence seq2;
-private static char nuc1;
-private static char nuc2;
-private static char nuc3;
-private static char nuc4;
+
+    private static DNASequence seq;
+    private static DNASequence seq2;
+    private static char nuc1;
+    private static char nuc2;
+    private static char nuc3;
+    private static char nuc4;
 
     public DNASequenceTest() {
     }
-    
+
     @Before
     public void setUpClass() {
-    seq = new DNASequence ("CGAT");
-    seq2 = new DNASequence ("CGTZ");
-    nuc1 = 'A';
-    nuc2 = 'C';
-    nuc3 = 'G';
-    nuc4 = 'T';
+        seq = new DNASequence("CGAT");
+        seq2 = new DNASequence("CGTZ");
+        nuc1 = 'A';
+        nuc2 = 'C';
+        nuc3 = 'G';
+        nuc4 = 'T';
     }
-    
+
     @After
     public void tearDownClass() {
-    seq = null;
-    seq2 = null;
+        seq = null;
+        seq2 = null;
     }
 
     /**
      * Test of isValid method, of class DNASequence.
      */
     @Test
-    public void testIsValid() {
+    public void testIsValid01() {
         System.out.println("Test: isValid");
         DNASequence instance = seq;
         DNASequence instance2 = seq2;
         boolean expResult = true;
-        boolean expResult2 = false;
         boolean result = instance.isValid();
-        boolean result2 = instance2.isValid();
-        System.out.println("Test1:\n    Resultado esperado: "+expResult+" -- Resultado Obtenido: "+result);
-        System.out.println("Test2:\n    Resultado esperado: "+expResult2+" -- Resultado Obtenido: "+result2);
+        System.out.println("Test1:\n    Resultado esperado: " + expResult + " -- Resultado Obtenido: " + result);
         assertEquals(expResult, result);
-        assertEquals(expResult2, result2);
+    }
+
+    /**
+     * Test of isValid method, of class DNASequence.
+     */
+    @Test
+    public void testIsValid02() {
+        System.out.println("Test: isValid");
+        DNASequence instance = seq;
+        DNASequence instance2 = seq2;
+        boolean expResult = false;
+        boolean result = instance2.isValid();
+        System.out.println("Test2:\n    Resultado esperado: " + expResult + " -- Resultado Obtenido: " + result);
+        assertEquals(expResult, result);
     }
 
     /**
@@ -78,14 +88,14 @@ private static char nuc4;
         int result2 = instance.count(CYTOSINE);
         int result3 = instance.count(THYMINE);
         int result4 = instance.count(GUANINE);
-        System.out.println("Test1:\n    Resultado esperado: "+expResult2+" -- Resultado Obtenido: "+result1);
-        System.out.println("Test2:\n    Resultado esperado: "+expResult1+" -- Resultado Obtenido: "+result2);
-        System.out.println("Test3:\n    Resultado esperado: "+expResult1+" -- Resultado Obtenido: "+result3);
-        System.out.println("Test4:\n    Resultado esperado: "+expResult1+" -- Resultado Obtenido: "+result4+"\n");
+        System.out.println("Test1:\n    Resultado esperado: " + expResult2 + " -- Resultado Obtenido: " + result1);
+        System.out.println("Test2:\n    Resultado esperado: " + expResult1 + " -- Resultado Obtenido: " + result2);
+        System.out.println("Test3:\n    Resultado esperado: " + expResult1 + " -- Resultado Obtenido: " + result3);
+        System.out.println("Test4:\n    Resultado esperado: " + expResult1 + " -- Resultado Obtenido: " + result4 + "\n");
         assertEquals(expResult2, result1);
         assertEquals(expResult1, result2);
         assertEquals(expResult1, result3);
         assertEquals(expResult1, result4);
     }
-    
+
 }
