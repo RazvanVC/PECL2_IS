c9d8a54076be70b3a1652a4924b20991cb6d8939
RazvanVC <razvanvc@yahoo.com>
2020-05-24 19:11:29 +0200

    Final

--- PECL2_IS/test/Ejercicio_2_ResolucionEasyMock/CajeroAutomaticoTest.java
+++ PECL2_IS/test/Ejercicio_2_ResolucionEasyMock/CajeroAutomaticoTest.java
@@ -5,131 +5,414 @@
  */
 package Ejercicio_2_ResolucionEasyMock;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import static org.easymock.EasyMock.*;
+
 /**
  *
  * @author razvanvc
  */
 public class CajeroAutomaticoTest {
-    
-    
-    private CajeroAutomatico classUnderTest;  
+
+    private CajeroAutomatico classUnderTest;
     private GestorPrestamosPersonales mock;
-    
+
     public CajeroAutomaticoTest() {
     }
-    
-    @Before
-    public void setUp(){
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
         classUnderTest = new CajeroAutomatico("1111111111");
-        classUnderTest.iniciarSesion("0000");           
-        /* (1) Crea un mock para la clase colaboradora*/         
+        classUnderTest.iniciarSesion("1234");
+        /* (1) Crea un mock para la clase colaboradora*/
         mock = createMock(GestorPrestamosPersonales.class);
     }
-    
-    @After
-    public void tearDown() {
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
         classUnderTest.cerrarSesion();
         reset(mock);
     }
 
-    @Test     
-    public void realizarPrestamoPersonal01() throws Exception{
-        System.out.println("realizarPrestamoPersonal01");
-        
-        expect(mock.PrestamosRecientes()).andReturn(false);         
-        expect(mock.CantidadValida(1500)).andReturn(true);
-        expect(mock.TiempoValido(10)).andReturn(true);
-        mock.RealizarPrestamo(1500, 10);
-        
-        replay(mock); 
-        
-        boolean result = classUnderTest.realizarPrestamoPersonal(mock, 1500, 10);         
+    /**
+     * Test of realizarPrestamoPersonal method, of class CajeroAutomatico.
+     */
+    
+    //4 MESES
+    @Test
+    public void testRealizarPrestamoPersonal() {
+        int Cantidad = 700;
+        int Meses = 4;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal2() {
+        int Cantidad = 700;
+        int Meses = 4;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal3() {
+        int Cantidad = 1200;
+        int Meses = 4;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(true);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal4() {
+        int Cantidad = 1200;
+        int Meses = 4;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(true);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal5() {
+        int Cantidad = 3200;
+        int Meses = 4;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal6() {
+        int Cantidad = 3200;
+        int Meses = 4;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    //7 MESES
+    @Test
+    public void testRealizarPrestamoPersonal7() {
+        int Cantidad = 700;
+        int Meses = 7;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(true);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal8() {
+        int Cantidad = 700;
+        int Meses = 7;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(true);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal9() {
+        int Cantidad = 1200;
+        int Meses = 7;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(true);
+        expect(mock.TiempoValido(Meses)).andReturn(true);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal10() {
+        int Cantidad = 1200;
+        int Meses = 7;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(true);
+        expect(mock.TiempoValido(Meses)).andReturn(true);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
         assertTrue(result);
-        
-        verify(mock);
     }
     
-    @Test     
-    public void realizarPrestamoPersonal02() throws Exception{
-        System.out.println("realizarPrestamoPersonal02");
-        
-        expect(mock.PrestamosRecientes()).andReturn(true);         
-        expect(mock.CantidadValida(1500)).andReturn(true);
-        expect(mock.TiempoValido(10)).andReturn(true);
-        mock.RealizarPrestamo(1500, 10);
-        
-        replay(mock); 
-        
-        boolean result = classUnderTest.realizarPrestamoPersonal(mock, 1500, 10);         
+    @Test
+    public void testRealizarPrestamoPersonal11() {
+        int Cantidad = 3200;
+        int Meses = 7;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(true);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
         assertFalse(result);
-        
     }
     
-    @Test     
-    public void realizarPrestamoPersonal03() throws Exception{
-        System.out.println("realizarPrestamoPersonal03");
-        
-        expect(mock.PrestamosRecientes()).andReturn(false);         
-        expect(mock.CantidadValida(4500)).andReturn(false);
-        expect(mock.TiempoValido(10)).andReturn(true);
-        mock.RealizarPrestamo(4500, 10);
-        
-        replay(mock); 
-        
-        boolean result = classUnderTest.realizarPrestamoPersonal(mock, 4500, 10);         
+    @Test
+    public void testRealizarPrestamoPersonal12() {
+        int Cantidad = 3200;
+        int Meses = 7;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(true);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
         assertFalse(result);
-        
     }
     
-    @Test     
-    public void realizarPrestamoPersonal04() throws Exception{
-        System.out.println("realizarPrestamoPersonal04");
-        
-        expect(mock.PrestamosRecientes()).andReturn(false);         
-        expect(mock.CantidadValida(1500)).andReturn(true);
-        expect(mock.TiempoValido(20)).andReturn(false);
-        mock.RealizarPrestamo(1500, 20);
-        
-        replay(mock); 
-        
-        boolean result = classUnderTest.realizarPrestamoPersonal(mock, 1500, 20);         
+    //MESES 14 MESES
+    
+    @Test
+    public void testRealizarPrestamoPersonal13() {
+        int Cantidad = 700;
+        int Meses = 14;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
         assertFalse(result);
-        
     }
     
-    @Test     
-    public void realizarPrestamoPersonal05() throws Exception{
-        System.out.println("realizarPrestamoPersonal05");
-        
-        expect(mock.PrestamosRecientes()).andReturn(false);         
-        expect(mock.CantidadValida(1500)).andReturn(true);
-        expect(mock.TiempoValido(5)).andReturn(false);
-        mock.RealizarPrestamo(1500, 5);
-        
-        replay(mock); 
-        
-        boolean result = classUnderTest.realizarPrestamoPersonal(mock, 1500, 5);         
+    @Test
+    public void testRealizarPrestamoPersonal14() {
+        int Cantidad = 700;
+        int Meses = 14;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
         assertFalse(result);
-        
     }
     
-    @Test     
-    public void realizarPrestamoPersonal06() throws Exception{
-        System.out.println("realizarPrestamoPersonal06");
-        
-        expect(mock.PrestamosRecientes()).andReturn(false);         
-        expect(mock.CantidadValida(500)).andReturn(false);
-        expect(mock.TiempoValido(10)).andReturn(true);
-        mock.RealizarPrestamo(500, 10);
-        
-        replay(mock); 
-        
-        boolean result = classUnderTest.realizarPrestamoPersonal(mock, 500, 10);         
+    @Test
+    public void testRealizarPrestamoPersonal15() {
+        int Cantidad = 1200;
+        int Meses = 14;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(true);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
         assertFalse(result);
     }
+    
+    @Test
+    public void testRealizarPrestamoPersonal16() {
+        int Cantidad = 1200;
+        int Meses = 14;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(true);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal17() {
+        int Cantidad = 3200;
+        int Meses = 14;
+        boolean Prestamo = true;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testRealizarPrestamoPersonal18() {
+        int Cantidad = 3200;
+        int Meses = 14;
+        boolean Prestamo = false;
+        System.out.println("Realizar Prestamo Personal: \n Variables: \n Cantidad = "
+                + Cantidad + "\nMeses = " + Meses + "\nPrestamo en ultimo año = " + Prestamo);
+
+        expect(mock.CantidadValida(Cantidad)).andReturn(false);
+        expect(mock.TiempoValido(Meses)).andReturn(false);
+        expect(mock.PrestamosRecientes()).andReturn(Prestamo);
+
+        mock.RealizarPrestamo(Cantidad, Meses);
+
+        replay(mock);
+
+        boolean result = classUnderTest.realizarPrestamoPersonal(mock, Cantidad, Meses);
+        assertFalse(result);
+    }
+
 }
